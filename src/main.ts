352b9b63840a5e3107ac9d74c8e7fadef6eed4d9
mpng <meijiao@gmail.com>
2023-02-27 10:52:11 +0800

    validate format and dups

---
+++ src/main.ts
@@ -0,0 +1,56 @@
+import * as core from "@actions/core";
+import { exec } from "@actions/exec";
+import { parseGitPatch } from "./parse";
+import { validateGitPatch } from "./validate";
+import { getValidated } from "./get-validated";
+import { ValidatedSet, ValidationError } from "./types/types";
+
+async function run(): Promise<void> {
+
+  let gitDiff = "";
+  let gitDiffError = "";
+
+  try {
+    await exec("git", ["diff", "origin/main", "-U0", "--color=never"], {
+      listeners: {
+        stdout: (data: Buffer) => {
+          gitDiff += data.toString();
+        },
+        stderr: (data: Buffer) => {
+          gitDiffError += data.toString();
+        },
+      },
+    });
+  } catch (error: any) {
+    core.debug(error.message)
+    core.setFailed(error.message);
+  }
+
+  if (gitDiffError) {
+    core.setFailed(gitDiffError);
+  }
+
+  core.info(`Git diff: ${gitDiff}`)
+  console.log(`Git diff: ${gitDiff}`)
+
+  let validatedSet: ValidatedSet;
+  try {
+    validatedSet = await getValidated();
+
+    const errors: ValidationError[][] = []
+    parseGitPatch(gitDiff).forEach((patch) => {
+      const patchErrors = validateGitPatch(patch, validatedSet);
+      if (patchErrors) {
+        errors.push(patchErrors);
+      }
+        
+    if (errors.length > 0) {
+      core.setFailed(errors.join(","));
+    }
+    });
+  } catch (error: any) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
